a8556bb898b464b80c7476167e0218035b84e0f7
SKILLED ENGINEER <157645772+skilledengineer7@users.noreply.github.com>
2024-03-09 20:14:48 +0530

    Add files via upload

---
+++ DDLJ_Arduino_SkilledEngineer.ino
@@ -0,0 +1,109 @@
+/*
+ DDLJ guitar tab on Arduino
+ by SKILLED ENGINEER | YouTube | Facebook | Instagram | Threads
+*/
+
+unsigned int tonePlay = 120; 
+unsigned long tonePause = 260;
+
+void setup() {
+  pinMode(11, OUTPUT); 
+}
+
+void loop() {
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 494, tonePlay);
+          delay(tonePause);
+          tone(11, 440, tonePlay);
+          delay(tonePause);
+          tone(11, 494, tonePlay);
+          delay(tonePause);
+          tone(11, 392, tonePlay);
+          delay(tonePause);
+          tone(11, 440, tonePlay);
+          delay(tonePause);
+          tone(11, 523, tonePlay);
+          delay(tonePause);
+          tone(11, 494, 300);
+          delay(tonePause);
+          delay(tonePause);
+          delay(tonePause);
+
+          //--------
+
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 494, tonePlay);
+          delay(tonePause);
+          tone(11, 440, tonePlay);
+          delay(tonePause);
+          tone(11, 494, tonePlay);
+          delay(tonePause);
+          tone(11, 392, tonePlay);
+          delay(tonePause);
+          tone(11, 440, tonePlay);
+          delay(tonePause);
+          tone(11, 392, tonePlay);
+          delay(tonePause);
+          tone(11, 370, 300);
+          delay(tonePause);
+          delay(tonePause);
+          delay(tonePause);
+          
+          //---------
+
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 370, tonePlay);
+          delay(tonePause);
+          tone(11, 294, tonePlay);
+          delay(tonePause);
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 370, tonePlay);
+          delay(tonePause);
+          tone(11, 294, tonePlay);
+          delay(tonePause);
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 392, tonePlay);
+          delay(tonePause);
+          tone(11, 370, 300);
+          delay(tonePause);
+          delay(tonePause);
+          delay(tonePause);
+
+          //-------
+
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 370, tonePlay);
+          delay(tonePause);
+          tone(11, 294, tonePlay);
+          delay(tonePause);
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 370, tonePlay);
+          delay(tonePause);
+          tone(11, 294, tonePlay);
+          delay(tonePause);
+          tone(11, 330, tonePlay);
+          delay(tonePause);
+          tone(11, 392, tonePlay);
+          delay(tonePause);
+          tone(11, 370, 300);
+          delay(tonePause);
+          tone(11, 370, 300);
+          delay(tonePause);
+          delay(tonePause);
+          delay(tonePause);            
+}
